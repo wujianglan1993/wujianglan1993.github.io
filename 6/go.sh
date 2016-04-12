64a83c45f7a5a83e3370f3c95ad0fdc895d7d0e4
wujianglan1993 <1453055591@qq.com>
2016-04-12 18:01:46 +0800

    新增上传脚本

---
+++ 6/go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "1453055591@qq.com"
+git config --global user.name "wujianglan1993"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://wujianglan1993.github.io
